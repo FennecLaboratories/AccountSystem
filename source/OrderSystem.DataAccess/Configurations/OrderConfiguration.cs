b59d5b0b4b1a2faf1b2ea0cc4c81ac7784492fa1
Kiralaine <k9034449@gmail.com>
2025-06-22 00:48:22 +0500

    mm there is some bugs in Order Entitiy Configuration cuz it not writed full,be carefull

---
+++ source/OrderSystem.DataAccess/Configurations/OrderConfiguration.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using OrderSystem.DataAccess.Entities;
+namespace OrderSystem.DataAccess.Configurations;
+public class OrderConfiguration :  
+{
+    
+}
